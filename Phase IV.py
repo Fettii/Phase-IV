68f02fb07726bc2da7f0851a47cb984b71cb2833
Yafet Tedla <65037439+Fettii@users.noreply.github.com>
2022-03-31 15:14:49 -0400

    Add files via upload

---
+++ Phase IV.py
@@ -0,0 +1,34 @@
+#this is where the general info about a basketball player
+class Players:
+    def __init__(self,name,number,age,pos,height,weight):
+        self.name = name
+        self.age = age
+        self.pos = pos 
+        self.height = height
+        self.weight = weight
+        self.number = number
+#this is where the genreal info about how that player performs on the basketball court
+class Roster:
+    def __init__(self,points,blocks,steals,rebounds,assists,fg,ft,threepointer,turnovers,plus_minus):
+        self.points = points
+        self.blocks = blocks
+        self.steals = steals
+        self.rebounds = rebounds
+        self.assists = assists
+        self.fg = fg
+        self.ft = ft
+        self.threepointer = threepointer
+        self.turnovers = turnovers
+        self.plus_minus = plus_minus
+
+def main
+       
+
+
+
+
+
+
+
+
+
