56d1c4ca073eee6458709115c0a6e939c60830c7
Yafet Tedla <65037439+Fettii@users.noreply.github.com>
2022-04-11 17:43:49 -0400

    2

--- Coach_AI-Opt 1.py
+++ Coach_AI-Opt 1.py
@@ -1,13 +1,20 @@
+
+
 def read_roster():
-    pass
+    reader = open("Roster.txt","r")
+    print(reader.readlines())
+
+    
 def add_items():
     pass
+
+
 def edit_items():
     pass
 def remove_items():
     pass
 
-
+read_roster()
 
 
 
